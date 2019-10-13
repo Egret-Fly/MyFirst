a28f12d2ace7a68d0b35fc6b504b0860c3a4bed4
Egret-Fly <huangningyuan029@163.com>
2019-10-14 00:01:20 +0800

    1

---
+++ svm.py
@@ -0,0 +1,8 @@
+from svmutil import *
+
+y,x =svm_read_problem('F:\新建文件夹\dataset/train.txt')
+yt,xt=svm_read_problem('F:\新建文件夹\dataset/test.txt')
+model = svm_train(y,x)
+
+p_label, p_acc, p_val = svm_predict(yt[0:117], xt[0:117], model)
+print(p_label, p_acc, p_val)
